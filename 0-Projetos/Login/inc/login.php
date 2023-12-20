9d5a6bc1585fe05751346af8692462d820af4627
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2023-12-20 14:48:12 -0300

    SignUp part done

---
+++ 0-Projetos/Login/inc/login.php
@@ -0,0 +1,10 @@
+<?php
+
+    if(isset($_POST['submit'])) {
+
+        $name = $_POST['username'];
+        $pass = $_POST['password'];
+        $passRepeat = $_POST['passwordRepeat'];
+        $email = $_POST['email'];
+
+    }
