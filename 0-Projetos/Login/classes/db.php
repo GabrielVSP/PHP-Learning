2eff74f3032b8826657dc2c1a6683398e7074a1f
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2023-12-20 18:35:02 -0300

    Added UTF-8 to PDO

--- 0-Projetos/Login/classes/db.php
+++ 0-Projetos/Login/classes/db.php
@@ -11,7 +11,7 @@
                 $user = 'root';
                 $pass = '';
 
-                $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
                 return $pdo;
 
